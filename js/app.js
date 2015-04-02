1cf60bc8abf502351cbf9fd9a19809846910b8f0
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-04-02 15:53:45 -0700

    no real changes

--- js/app.js
+++ js/app.js
@@ -1,2 +1,2 @@
 angular
-	.module('mainApp', ['firebase']);
+	.module('mainApp', []);
--- js/controller.js
+++ js/controller.js
@@ -2,9 +2,7 @@ angular
 	.module('mainApp')
 	.controller('TicTacToeController', TicTacToeController);
 
-TicTacToeController.$inject = ['$firebaseObject'];
-
-function TicTacToeController($firebaseObject) {
+function TicTacToeController() {
 	
 	var self = this;
 	self.test = "controller is working";
@@ -12,77 +10,29 @@ function TicTacToeController($firebaseObject) {
 
 	self.readyToPlay = readyToPlay;
 	self.alternatePlay = alternatePlay;
-	
 	self.determineWinner = determineWinner;
+	self.restartGameOver = restartGameOver;
 	self.allThree = allThree;
 	self.winnerIs = winnerIs;
 	self.winsRow = winsRow;
 	self.winsCol = winsCol;
 	self.winsDiag = winsDiag;
 
-	//richard was here
-	self.gameObj = syncFB();
-
-	function syncFB(){
-		var ref = new Firebase('https://tictact.firebaseio.com/');
-		var gameObj = $firebaseObject(ref);
-
-		gameObj.$loaded(function(){
-			//all thee shit
-			gameObj.nextPlayer;
-			//end of shit
-			gameObj.board = [{piece : " "},
-						{piece : " "},
-						{piece : " "},
-						{piece : " "},
-						{piece : " "},
-						{piece : " "},
-						{piece : " "},
-						{piece : " "},
-						{piece : " "}];
-			gameObj.$save();
-		});
-		return gameObj;
+	self.board = [];
+	for (var i = 0 ; i < 9 ; i++) {
+			self.board.push({piece : "", space: i})
 	}
-	//end of richard
-
-	// self.board = [];
-	// for (var i = 0 ; i < 9 ; i++) {
-	// 		self.board.push({piece : " ", space: i})
-	// }
-
-	// function getBoardObject() {
-	// 	var ref = new Firebase("https://tictact.firebaseio.com/")
-	// 	var board = $firebaseObject(ref);
-	// 	board.spaces = [{piece : " "},
-	// 					{piece : " "},
-	// 					{piece : " "},
-	// 					{piece : " "},
-	// 					{piece : " "},
-	// 					{piece : " "},
-	// 					{piece : " "},
-	// 					{piece : " "},
-	// 					{piece : " "}]
-	// 	// board.$save();
-	// 	board.$loaded(function() {
-	// 		console.log("hi")
-	// 			board.piece
-	// 	});
-	// 	return board;
-
-	// }
 
 // Ready to Play will script an alert stating X goes first and then clears the board of placed pieces.
 	function readyToPlay() {
-		alert("Player X goes first");
 		for (var i = 0; i < 9; i ++){
-		self.gameObj.board[i].piece = " ";
+		self.board[i].piece = "";
 		}	
 	}
 // Alternate Play will alternate between X and O. First move will be X, then O upon click.
 	function alternatePlay(space) {
-		var index = self.gameObj.board.indexOf(space);	
-		if (self.gameObj.board[index].piece !== " ") {
+		var index = self.board.indexOf(space);	
+		if (self.board[index].piece !== "") {
 			alert("That space is occupied already");
 		}
 		else {
@@ -93,12 +43,13 @@ function TicTacToeController($firebaseObject) {
 				nextPlayer = "O";
 			}
 
-			self.gameObj.board[index].piece = nextPlayer;
-			console.log("hi", self.gameObj.board[index].piece);
+			self.board[index].piece = nextPlayer;
+			console.log("hi", self.board[index].piece);
 
 		}
 		self.determineWinner();
-	}
+		
+		}
 
 	function determineWinner() {
 		if (winnerIs("X")) {
@@ -109,37 +60,48 @@ function TicTacToeController($firebaseObject) {
 			setTimeout(function(){
 				alert("O wins"),500});
 		}
-		else {
-			 null;
-		}		
-	}
-
-	function allThree(player, spaceOne,spaceTwo,spaceThree) {
-		return (spaceOne === player) && (spaceTwo ===player) && (spaceThree === player);
+		else if (winnerIs("X") !== true && winnerIs("O") !== true) {
+			console.log("no winner");
+		}
+		restartGameOver();
+		
+		}
+	function restartGameOver() {
+		if (winnerIs("X") || winnerIs("O")) {
+			setTimeout(function() {
+				alert("Game Over, Click to Play Again"),1000});
+			for (var i = 0; i < 9; i ++){
+				console.log(i);
+				self.board[i].piece = "";
+			};
+		}
 	}
 
 	function winnerIs(player) {
 		return winsRow(player) || winsCol(player) || winsDiag(player);
 	}
 
+	function allThree(player, spaceOne,spaceTwo,spaceThree) {
+		return (spaceOne === player) && (spaceTwo ===player) && (spaceThree === player);
+	}
+
 	function winsRow(player, spaceOne,spaceTwo,spaceThree) {
-		return  allThree(player, self.gameObj.board[0].piece, self.gameObj.board[1].piece, self.gameObj.board[2].piece) ||
-			allThree(player, self.gameObj.board[3].piece, self.gameObj.board[4].piece, self.gameObj.board[5].piece) ||
-			allThree(player, self.gameObj.board[6].piece, self.gameObj.board[7].piece, self.gameObj.board[8].piece);
+		return  allThree(player, self.board[0].piece, self.board[1].piece, self.board[2].piece) ||
+			allThree(player, self.board[3].piece, self.board[4].piece, self.board[5].piece) ||
+			allThree(player, self.board[6].piece, self.board[7].piece, self.board[8].piece);
 	}
 
 	function winsCol(player, spaceOne,spaceTwo,spaceThree) {
-		return allThree(player, self.gameObj.board[0].piece, self.gameObj.board[3].piece, self.gameObj.board[6].piece) ||
-			allThree(player, self.gameObj.board[1].piece, self.gameObj.board[4].piece, self.gameObj.board[7].piece) ||
-			allThree(player, self.gameObj.board[2].piece, self.gameObj.board[5].piece, self.gameObj.board[8].piece);
+		return allThree(player, self.board[0].piece, self.board[3].piece, self.board[6].piece) ||
+			allThree(player, self.board[1].piece, self.board[4].piece, self.board[7].piece) ||
+			allThree(player, self.board[2].piece, self.board[5].piece, self.board[8].piece);
 	}
-
+	
 	function winsDiag(player, spaceOne,spaceTwo,spaceThree) {
-		return allThree(player, self.gameObj.board[0].piece, self.gameObj.board[4].piece, self.gameObj.board[8].piece) ||
-			allThree(player, self.gameObj.board[2].piece, self.gameObj.board[4].piece, self.gameObj.board[6].piece);
+		return allThree(player, self.board[0].piece, self.board[4].piece, self.board[8].piece) ||
+			allThree(player, self.board[2].piece, self.board[4].piece, self.board[6].piece);
 	}
+
 }
-
-
 
 
