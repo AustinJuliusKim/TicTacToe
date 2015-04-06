390b9c8ddc26b2fb512afa6e3f4233e4ecb9b62a
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-04-05 23:39:52 -0700

    Cleaned up and delted main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// var space = document.getElementsByClassName("space");	
-
-
-var space = document.querySelectorAll(".space");
-
-
-
-// space[i].style.backgroundColor = ""
-// Will change the background color of spaces that win
